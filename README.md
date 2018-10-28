1c50edf5d34d3e1b4c70f6739229738732857e51
Martin Kapal <flamecze@gmail.com>
2018-10-28 16:03:35 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Arduino Remote Temperature
+
+## Hardware
+
+- Arduino UNO Rev3
+- Arduino Ethernet Shield W5100 R3
+- DS18B20 Waterproof Temperature Sensor
+
+## 3rd party libraries
+
+- DallasTemperature
+- OneWire
+- WebSocketClient (<https://github.com/flamecze/Arduino-Websocket>)
+
+## Configuration
+
+- Do `cp config.h.example config.h` and edit the WebSocket server name and port.
+- Compile
+- Upload
+- ...
+- Profit!
---
+++ remote-temperature-arduino.ino
@@ -0,0 +1,93 @@
+#include <SPI.h>
+#include <Ethernet.h>
+#include <WebSocketClient.h> // https://github.com/flamecze/Arduino-Websocket
+#include <Streaming.h>
+#include <DallasTemperature.h>
+#include <OneWire.h>
+#include "config.h"
+
+EthernetClient client;
+WebSocketClient webSocketClient;
+
+byte mac[] = { 0xDE, 0xAB, 0xBE, 0xEF, 0xFE, 0xEF };
+const char hostname[] = HOSTNAME;
+const int port = PORT;
+
+#define ONE_WIRE_BUS 5
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensors(&oneWire);
+
+float tempC = 0;
+char tempStr[6];
+
+bool connected = false;
+
+#define delayMillis 1000UL
+
+unsigned long thisMillis = 0;
+unsigned long lastMillis = 0;
+
+void setup() {
+  // disable SD SPI
+  pinMode(4, OUTPUT);
+  digitalWrite(4, HIGH);
+
+  Serial.begin(9600);
+  Serial.println(F("Starting..."));
+  Ethernet.begin(mac); // initialize ethernet
+  Serial.println(Ethernet.localIP());
+  delay(300);
+
+  sensors.begin();
+  connect();
+}
+
+void loop() {
+  thisMillis = millis();
+
+  if (thisMillis - lastMillis > delayMillis) {
+    lastMillis = thisMillis;
+
+    sensors.requestTemperatures();
+    tempC = sensors.getTempCByIndex(0);
+
+    dtostrf(tempC, 4, 2, tempStr);
+
+    if (client.connected()) {
+      webSocketClient.sendData(tempStr);
+    } else {
+      Serial.println("Client disconnected.");
+      connected = false;
+      connect();
+    }
+  }
+}
+
+void connect() {
+  while(!connected) {
+    Serial.println("Connecting");
+    // Connect to the websocket server
+    if (client.connect(HOSTNAME, PORT)) {
+      Serial.println("Connected");
+      connected = true;
+    } else {
+      Serial.println("Connection failed.");
+    }
+    delay(1000);
+  }
+  
+  char fullServerName[40];
+  
+  sprintf(fullServerName,"%s:%i", HOSTNAME, PORT);
+
+  // Handshake with the server
+  webSocketClient.path = "/";
+  webSocketClient.host = fullServerName;
+
+  while(!webSocketClient.handshake(client)) {
+    Serial.println("Handshake failed.");
+    delay(1000);
+  }
+  Serial.println("Handshake successful");
+}
+
